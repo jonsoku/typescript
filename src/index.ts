d245756624dda7f17a520732f92a3f495a57d65d
jongseok <the2792@gmail.com>
2019-03-04 01:38:42 +0900

    class

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,23 @@
 console.log("hello");
 
-interface Human{
-    name: string;
-    age:number;
-    gender:string;
+class Human {
+    public name: string;
+    public age: number;
+    public gender: string;
+    constructor(name:string, age:number, gender?:string){
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+    }
 }
 
-const person = {
-    name: "nicolas",
-    gender: "male",
-    age: 22
-};
-
+const kazuko = new Human("KAZUKO", 20);
 
 /* 변수뒤에 ?를 붙히면 required속성이 풀린다. (빠져도 된다는 말.) */
 const sayHi = (person: Human):string => {
     return `hello ${person.name}, you are ${person.age}, you are a ${person.gender}`;
 }
 
-console.log(sayHi(person));
+console.log(sayHi(kazuko));
 
 export{};
